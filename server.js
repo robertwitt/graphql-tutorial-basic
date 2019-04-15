03670694c98d65f1e6d4abe0612ea6dec1c2ba14
Robert Witt <robert.witt@zalando.de>
2019-04-15 07:37:47 +0200

    Setup Apollo server.

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 import express from 'express';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import bodyParser from 'body-parser';
+
 const server = express();
+
+server.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql'
+}));
+
+server.use('/graphql', bodyParser.json(), graphqlExpress({}));
 
 server.listen(4000, () => {
     console.log('listening on port 4000');
